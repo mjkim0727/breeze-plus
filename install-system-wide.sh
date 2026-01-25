f825d01ae11052d37cc59588612a42c1a1e5aca0
마리임 <78864678+mjkim0727@users.noreply.github.com>
2026-01-25 16:05:46 +0900

    Update install-system-wide.sh

--- install-system-wide.sh
+++ install-system-wide.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo cp ../src/breeze-plus* /usr/share/icons;
+sudo cp -r src/breeze-plus* /usr/share/icons;
 sudo gtk-update-icon-cache -f -t /usr/share/icons/breeze-plus* && xdg-desktop-menu forceupdate;
 
