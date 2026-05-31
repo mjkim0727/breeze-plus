549a9c800b8250309c6eae2fbd13e2e972da6a75
마리임 <78864678+mjkim0727@users.noreply.github.com>
2026-05-31 23:12:21 +0900

    Update install-local.sh

--- install-local.sh
+++ install-local.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cp -r ../src/breeze-plus* ~/.local/share/icons;
+cp -r src/breeze-plus* ~/.local/share/icons;
 gtk-update-icon-cache -f -t ~/.local/share/icons/breeze-plus* && xdg-desktop-menu forceupdate;
 
